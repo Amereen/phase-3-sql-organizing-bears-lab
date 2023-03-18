a593ea7c52865911955600b46c3f1f48e54d21f7
Amereen Hanvel <amereenhanvel786@gmail.com>
2023-03-17 20:56:50 -0400

    commit tree d0f25e0065f9428c8f032a2c906a44a554513bb1

---
+++ lib/insert.sql
@@ -0,0 +1,38 @@
+
+
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 5, 'M', 'brown', 'grumpy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 7, 'M', 'black', 'energetic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 3, 'F', 'white', 'shy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 9, 'M', 'brown', 'brave', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 2, 'F', 'black', 'playful', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 6, 'M', 'green', 'grumpy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 4, 'F', 'brown', 'friendly', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 0, '', '', '', 0);
+
+
+
+
+
+
+
+
+
+
+
